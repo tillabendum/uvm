c45401d8d9601aa1c6432224ab363218ba8441f3
zbp4zb <Danil.Bychkov@onsemi.com>
2022-02-09 06:11:00 -0800

    One transaction flow in deployed in time

--- my_sequence.sv
+++ my_sequence.sv
@@ -1,15 +1,38 @@
 class my_sequence extends uvm_sequence #( my_seq_item );
 `uvm_object_utils( my_sequence );
 
+
+rand int pauses[];
+
+constraint num_items_ct{
+  pauses.size inside { [1:10] };
+}
+
+constraint pause_range_ct{
+  foreach( pauses[i] )
+    pauses[i] inside { [1:5] };
+}
+
 function new( string name = "" );
    super.new( name );
 endfunction: new
 
+task pre_start;
+  randomize();
+endtask
+
 task body;
-  my_seq_item req;
-  req = new;
-  start_item( req );
-  finish_item( req );
+  foreach( pauses[i] )
+    begin
+      my_seq_item req;
+      repeat( pauses[i] )
+        #1ns;
+      req = new;
+      `uvm_info( "Item created", "", UVM_MEDIUM )
+      start_item( req );
+      `uvm_info( "Item taken", "", UVM_MEDIUM )
+      finish_item( req );
+    end
 endtask
 
 endclass
