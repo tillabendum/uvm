26dac7151e522117c488cf960cdc61f0d9b109cb
zbp4zb <Danil.Bychkov@onsemi.com>
2022-08-17 01:53:43 -0700

    Driver and monitor are working in agent

--- my_env.sv
+++ my_env.sv
@@ -2,10 +2,8 @@ class my_env extends uvm_env;
 `uvm_component_utils( my_env )
 
 
-uvm_sequencer #( mem_req_item )   sequencer;
-mem_ladder_seq ladder;
-mem_drv  drv;
-
+mem_ladder_seq  ladder;
+mem_ag          ag;
 
 function new ( string name = "my_env", uvm_component parent = null );
   super.new( name, parent );
@@ -13,20 +11,18 @@ endfunction: new
 
 virtual function void build_phase( uvm_phase phase );
   super.build_phase( phase );
-  sequencer = new( "sequencer", this );
   ladder = mem_ladder_seq::type_id::create( "ladder", this );
-  drv = mem_drv::type_id::create( "drv", this );
+  ag = mem_ag::type_id::create( "ag", this );
 endfunction : build_phase
 
 virtual function void connect_phase( uvm_phase phase );
   super.connect_phase( phase );
-  drv.seq_item_port.connect( sequencer.seq_item_export );
 endfunction
 
 
 task run_phase( uvm_phase phase );
   phase.raise_objection( this );
-  ladder.start( sequencer );
+  ladder.start( ag.sequencer );
   phase.drop_objection( this );
 
 endtask : run_phase
