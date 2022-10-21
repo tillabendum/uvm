072d4c8796d7cb22e35a2d9f96e77dd30c65d7e1
zbp4zb <Danil.Bychkov@onsemi.com>
2022-10-21 19:18:20 +0300

    Fully runnable on VCS

---
+++ run.sh
@@ -0,0 +1,11 @@
+vlogan -full64 -ntb_opts uvm-1.2 -sverilog
+vlogan -full64 -ntb_opts uvm-1.2 -sverilog my_params_pkg.sv
+vlogan -full64 -ntb_opts uvm-1.2 -sverilog my_pkg.sv
+
+vlogan -full64 -ntb_opts uvm-1.2 -sverilog mem.sv
+vlogan -full64 -ntb_opts uvm-1.2 -sverilog mem_if.sv
+vlogan -full64 -ntb_opts uvm-1.2 -sverilog mem_bfm.sv
+vlogan -full64 -ntb_opts uvm-1.2 -sverilog top.sv
+
+vcs -full64 -ntb_opts uvm-1.2 -sverilog -LDFLAGS -Wl,--no-as-needed top
+./simv +UVM_TESTNAME=my_test
