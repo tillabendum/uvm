ab535a17f9dd210cd6f1495a3faf75d9cfb22d9e
Danil Bychkov <danil.bychkov@syntacore.com>
2024-05-07 16:45:45 +0300

    Checked write-read and peek-poke

--- src/my_seq.sv
+++ src/my_seq.sv
@@ -15,25 +15,38 @@ task body;
 
    write_data = 12;
    ral.alpha.write(
-      .status(status),
-      .value(write_data),
-      .parent(this)
+     .status(status),
+     .value(write_data),
+     .parent(this)
    );
-   `uvm_info("wr", $sformatf("Written data %0h in haos.alfa with status %s", write_data, status.name), UVM_NONE)
+   `uvm_info("write", $sformatf("Written data %0h in haos.alfa with status %s", write_data, status.name), UVM_NONE)
 
    ral.beta.read(
-      .status(status),
-      .value(read_data),
-      .parent(this)
-      );
-   `uvm_info("rd", $sformatf("Read data %0h from haos.beta with status %s", read_data, status.name), UVM_NONE)
+     .status(status),
+     .value(read_data),
+     .parent(this)
+   );
+   `uvm_info("read", $sformatf("Read data %0h from haos.beta with status %s", read_data, status.name), UVM_NONE)
+   if(read_data != (write_data + 1)) begin
+     `uvm_fatal("write-read", $sformatf("Beta register must me alpha register + 1. In fact alpha=%0d, beta=%0d", write_data, read_data))
+   end
 
    write_data = 8'hce;
    ral.alpha.poke(
-      .status(status),
-      .value(write_data),
-      .parent(this)
+     .status(status),
+     .value(write_data),
+     .parent(this)
    );
+
+   ral.beta.peek(
+     .status(status),
+     .value(read_data),
+     .parent(this)
+   );
+
+   if(read_data != (write_data + 1)) begin
+      `uvm_fatal("poke-peek", $sformatf("Beta register must me alpha register + 1. In fact alpha=%0d, beta=%0d", write_data, read_data))
+   end
 
 endtask
 
