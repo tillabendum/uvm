fb23beb956ffd5d22e0796cdceee66807deb86f8
Danil Bychkov <danil.bychkov@yadro.com>
2025-12-09 08:41:08 +0000

    Most automatic run

--- my_env.sv
+++ my_env.sv
@@ -1,7 +1,8 @@
 class my_env extends uvm_env;
 `uvm_component_utils( my_env )
 
-parameter int unsigned WIDTHS[2] = '{3, 5};
+parameter int unsigned WIDTHS[2]   = '{3, 5};
+parameter int unsigned WIDTHS_SIZE = 2;
 
 my_comp_abstract my_comps[];
 
@@ -20,18 +21,24 @@ endtask
 virtual function void build_phase( uvm_phase phase );
   super.build_phase( phase );
 
-  my_comps = new[2];
+  my_comps = new[WIDTHS_SIZE];
 
-  my_comps[0] = my_comp#(WIDTHS[0])::type_id::create("my_comps_0", this);
-  my_comps[1] = my_comp#(WIDTHS[1])::type_id::create("my_comps_1", this);
+  if(0 < WIDTHS_SIZE) my_comps[0] = my_comp#(WIDTHS[0])::type_id::create("my_comps_0", this);
+  if(1 < WIDTHS_SIZE) my_comps[1] = my_comp#(WIDTHS[1])::type_id::create("my_comps_1", this);
+  if(2 < WIDTHS_SIZE) my_comps[2] = my_comp#(WIDTHS[2])::type_id::create("my_comps_2", this);
+  if(3 < WIDTHS_SIZE) my_comps[3] = my_comp#(WIDTHS[3])::type_id::create("my_comps_3", this);
+  if(4 < WIDTHS_SIZE) my_comps[4] = my_comp#(WIDTHS[4])::type_id::create("my_comps_4", this);
+  if(5 < WIDTHS_SIZE) my_comps[5] = my_comp#(WIDTHS[5])::type_id::create("my_comps_5", this);
+  if(6 < WIDTHS_SIZE) my_comps[6] = my_comp#(WIDTHS[6])::type_id::create("my_comps_6", this);
+  if(7 < WIDTHS_SIZE) my_comps[7] = my_comp#(WIDTHS[7])::type_id::create("my_comps_7", this);
 
   imp = new("name", this);
 endfunction : build_phase
 
 virtual function void connect_phase( uvm_phase phase );
-  my_comps[0].port.connect(imp);
-  my_comps[1].port.connect(imp);
-
+  foreach (my_comps[i]) begin
+    my_comps[i].port.connect(imp);
+  end
 endfunction
 
 endclass : my_env
